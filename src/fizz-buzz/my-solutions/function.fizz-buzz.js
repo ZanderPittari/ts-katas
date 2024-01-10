edacbaf11b8d03f6c8fb867470e1962618ee2b66
Zander Pittari <zanderpittari@gmail.com>
2024-01-10 22:21:19 +1100

    test

--- src/fizz-buzz/my-solutions/function.fizz-buzz.js
+++ src/fizz-buzz/my-solutions/function.fizz-buzz.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.alterNumbers = void 0;
 function alterNumbers(inputNumber) {
-    if ((inputNumber % 3 == 0) && (inputNumber % 5 == 0)) {
-        inputNumber = "fizzbuzz";
+    if (inputNumber % 3 == 0 && inputNumber % 5 == 0) {
+        inputNumber = 'fizzbuzz';
     }
     else if (inputNumber % 5 == 0) {
-        inputNumber = "buzz";
+        inputNumber = 'buzz';
     }
     else if (inputNumber % 3 == 0) {
-        inputNumber = "fizz";
+        inputNumber = 'fizz';
     }
     else {
         inputNumber = inputNumber;
--- src/fizz-buzz/my-solutions/main.fizz-buzz.js
+++ src/fizz-buzz/my-solutions/main.fizz-buzz.js
@@ -4,12 +4,9 @@ var function_fizz_buzz_1 = require("./function.fizz-buzz");
 var arrayNumbers = [];
 var max = 100;
 var min = 50;
-for (var i = 0; i < (Math.floor(Math.random() * (max - min + 1) + min)); i++) {
+for (var i = 0; i < Math.floor(Math.random() * (max - min + 1) + min); i++) {
     arrayNumbers.push(Math.floor(Math.random() * (max - min + 1)) + min);
 }
-
-console.log(arrayNumbers)
-
 for (var i = 0; i < arrayNumbers.length; i++) {
     console.log((0, function_fizz_buzz_1.alterNumbers)(arrayNumbers[i]));
 }
