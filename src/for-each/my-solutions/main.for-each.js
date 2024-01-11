be8d56279e372f725caae7b90b61b87efd5e5bd2
Zander Pittari <zanderpittari@gmail.com>
2024-01-11 19:45:49 +1100

    test

--- src/for-each/my-solutions/main.for-each.js
+++ src/for-each/my-solutions/main.for-each.js
@@ -6,7 +6,7 @@ var min = 50;
 function elementCallback(value, element) {
     console.log(element);
 }
-for (var i = 0; i < (Math.floor(Math.random() + (max - min + 1)) + min); i++) {
+for (var i = 0; i < Math.floor(Math.random() + (max - min + 1)) + min; i++) {
     arrayNumber.push(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 arrayNumber.forEach(elementCallback);
